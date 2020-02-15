5299630276bbfcffa4765dca5127d6f13be19333
Henrylle Maia <henryllemaia@gmail.com>
2020-02-15 17:46:44 -0300

    Checando sorriso

--- smile-detector/smile-detector.py
+++ smile-detector/smile-detector.py
@@ -6,28 +6,62 @@ face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_defau
 eye_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml') 
 smile_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_smile.xml') 
 
+def has_smile (gray, frame):
+    faces = face_cascade.detectMultiScale(gray,1.3,5)
+    tem_sorriso=False
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x,y), ((x+w), (y+h)), (255,0,0),2)
+        roi_gray=gray[y:y+h, x:x+w]
+        roi_color=frame[y:y+h, x:x +w]
+        smiles=smile_cascade.detectMultiScale(roi_gray,1.8,20)        
+        for(sx,sy,sw,sh) in smiles:                    
+          if sx>=0:
+            tem_sorriso=True
+          cv2.rectangle(roi_color, (sx, sy), ((sx + sw), (sy + sh)), (0, 0, 255), 2)
+
+    return tem_sorriso
+
 def detect (gray, frame):
     faces = face_cascade.detectMultiScale(gray,1.3,5)
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x,y), ((x+w), (y+h)), (255,0,0),2)
         roi_gray=gray[y:y+h, x:x+w]
         roi_color=frame[y:y+h, x:x +w]
-        smiles=smile_cascade.detectMultiScale(roi_gray,1.8,20)
-        
-        for(sx,sy,sw,sh) in smiles:
+        smiles=smile_cascade.detectMultiScale(roi_gray,1.8,20)        
+        for(sx,sy,sw,sh) in smiles:                              
           cv2.rectangle(roi_color, (sx, sy), ((sx + sw), (sy + sh)), (0, 0, 255), 2)
 
     return frame
 
-path_image='stage/foto.jpg'
+path_image_sem_sorriso='stage/IMG_4067.JPG'
+path_image_com_sorriso='stage/foto.jpg'
 
-img = cv2.imread(path_image)
-gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+print ('Imagem sem sorriso')
+img_1 = cv2.imread(path_image_sem_sorriso)
+gray=cv2.cvtColor(img_1,cv2.COLOR_BGR2GRAY)
 
-canvas = detect(gray,img)
 
-cv2.namedWindow('Foto', cv2.WINDOW_GUI_EXPANDED)
-cv2.imshow('Foto', canvas)
+if has_smile(gray,img_1):
+  print (' Encontrei um sorriso')
+  canvas = detect(gray,img_1)
+  cv2.namedWindow('Foto1', cv2.WINDOW_GUI_EXPANDED)
+  cv2.imshow('Foto1', canvas)
+else:
+  print (' Nada de sorriso por aqui')  
+
+
+print ('Imagem com sorriso')
+img_2 = cv2.imread(path_image_com_sorriso)
+gray=cv2.cvtColor(img_2,cv2.COLOR_BGR2GRAY)
+
+if has_smile(gray,img_1):
+  print (' Encontrei um sorriso')
+  canvas = detect(gray,img_2)
+  cv2.namedWindow('Foto2', cv2.WINDOW_GUI_EXPANDED)
+  cv2.imshow('Foto2', canvas)
+else:
+  print (' Nada de sorriso por aqui')  
+
 
 #Salvar imagem
 #cv2.imwrite('foto_marcada.png',img)
